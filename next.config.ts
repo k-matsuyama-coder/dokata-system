8e042335f71854db4fea270e870b152d1aa96ae5
松山海里 <kairimatsuyama@MacBook-Pro.local>
2026-03-13 12:32:09 +0900

    ignore typescript build errors

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,12 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
