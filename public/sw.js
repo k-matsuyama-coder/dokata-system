14a81c619cf7bdd59500656c45ae2249876db343
松山海里 <kairimatsuyama@MacBook-Pro.local>
2026-06-19 16:56:48 +0900

    fix duplicate push notification

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,12 @@
-self.addEventListener("push", (event) => {
+self.addEventListener("install", () => {
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", (event) => {
+    event.waitUntil(clients.claim());
+  });
+  
+  self.addEventListener("push", (event) => {
     const data = event.data ? event.data.json() : {};
   
     event.waitUntil(
